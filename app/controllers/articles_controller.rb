9ea90a5b6829d765d17f731474c130c71a688690
Kemal AKIN <kmlakin97@gmail.com>
2018-07-21 19:46:28 +0300

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,51 @@
 class ArticlesController < ApplicationController
+	# Article index all tüm verileri döndürür
+	def index
+    @articles = Article.all
+  	end
+	def show
+    	@article = Article.find(params[:id])
+  	end
+  	def edit
+  		@article = Article.find(params[:id])
+	end
 	def new
+		@article = Article.new
 	end
 
+	#def create
+  	#	@article = Article.new(params.require(:article).permit(:title, :text))
+ 
+  	#	@article.save
+  	#	redirect_to @article
+	#end
+
+	## Daha Temiz Şekilde Yazımı Permit alacağımız parametleri belirtir
 	def create
-		render plain: params[:article].inspect
+	  @article = Article.new(article_params)
+	 
+	  if @article.save
+	  redirect_to @article
+	  else
+	  	render 'new'
+	  end
+
 	end
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+	 
+	private
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
+
+	   
 end
